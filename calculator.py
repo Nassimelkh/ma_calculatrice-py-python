48563d1f0dedc57119eba515ca9e6d095f62ab21
nassimelkh <elkhider.nassim@gmail.com>
2024-10-16 13:58:42 +0100

    add calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+#### calculator ####
+
+def ajouter(a,b):
+    return a + b 
+
+def soustraire(a,b):
+    return a - b 
+
+
+def multiplier(a,b):
+    return a * b 
+
+def diviser(a,b):
+    return a / b 
+
+
